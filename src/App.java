24fa27a50d68a49b6f4fec4c068e90f245fef49b
SantiagoMc2021 <santiago.madariaga@utec.edu.pe>
2021-12-04 21:37:05 -0500

    Merging the two functions

--- src/App.java
+++ src/App.java
@@ -1,12 +1,13 @@
 import org.apache.jena.query.*;
-// import org.apache.jena.rdf.model.Literal;
-// import org.apache.jena.rdf.model.Model;
-// import org.apache.jena.util.FileManager;
+import org.apache.jena.rdf.model.Literal;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.util.FileManager;
 
 class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         sparqlTest();
+        FOF();
     }
 
     static void sparqlTest() {
@@ -42,4 +43,26 @@ class Main {
             qexec.close();
         }
     }
+
+    static void FOF() {
+
+        // FileManager.get().AddLocatorClassLoader(Main.class.getClassLoader());
+        Model model = FileManager.get().loadModel("archivo.rdf");
+        String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#>" +
+                "PREFIX foaf: <http://xmlns.com/foaf/0.1/>" +
+                "SELECT * WHERE{?person foaf:name ?x .}";
+        Query query = QueryFactory.create(queryString);
+        QueryExecution pexec = QueryExecutionFactory.create(query, model);
+        try {
+            var results = pexec.execSelect();
+            while (results.hasNext()) {
+                QuerySolution soln = results.nextSolution();
+                Literal name = soln.getLiteral("x");
+                System.out.println(name);
+            }
+        } finally {
+            pexec.close();
+        }
+    }
+
 }
